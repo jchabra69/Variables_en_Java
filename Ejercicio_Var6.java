e0084cbbb9080bfc5884174bf3c0d5a4d91d152a
Nico <114468991+jchabra595r@users.noreply.github.com>
2022-10-23 21:25:10 +0200

    Add files via upload
    Reemplazo integers por doubles

---
+++ Ejercicio_Var6.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.politecnicomalaga.ejercicios_var;
+
+/** EJERCICIO 6
+ * Escribe un programa que calcule el total de una factura a partir de la base imponible (precio sin
+IVA). Todos los datos se leerán desde el teclado
+ * @author Jose-PC
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio_Var6 {
+    
+    public static void main(String[] args) {
+      Scanner entrada=new Scanner(System.in);
+      double base;
+      double IVA;
+      double resultado;
+      
+        System.out.println("Ingresa la base imponible");
+        
+            base=entrada.nextDouble();
+            
+                System.out.println("Ahora el porcentaje");
+                
+                    IVA=entrada.nextDouble();
+                    
+                    
+                        System.out.println("El total a pagar en tu factura es: ");
+                            
+                                resultado= IVA*base/100;
+                                System.out.println(base+resultado);
+                                
+      
+    }
+    
+}
